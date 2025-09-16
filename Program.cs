9c91a6f7a0acbae78a1be977de6dca809f4fd335
Tim Leung <timhk.leung@gmail.com>
2025-09-16 18:53:04 +0800

    For CORS Allow Origin

--- Program.cs
+++ Program.cs
@@ -18,10 +18,23 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         ?? "Data Source=customers.db"));
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy
+            .WithOrigins("http://localhost:4200") // Angular dev server
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAngular");
 
 // Auto-migrate (for dev/demo)
 using (var scope = app.Services.CreateScope())
